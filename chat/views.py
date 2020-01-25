0584876489c72b907eb0539c9076aa2ef798c785
ashish1sasmal <ashishsasmal1@gmail.com>
2020-01-25 22:23:26 +0530

    65%

--- chat/views.py
+++ chat/views.py
@@ -9,18 +9,17 @@ def home(request):
     return render(request,"chat/index.html")
 
 def message(request,id):
-
+    print("here!")
+    print(request.POST)
+    if request.POST.get('message')!='':
+        form=Message(sender=request.user, reciever=User.objects.get(id=id),text=request.POST.get('message'))
+        form.save()
+        print('sav')
+        messages.success(request,'message has been submitted!')
     return HttpResponse("")
 
 
 def chat(request,id):
-    if request.method=="POST":
-        print("here!")
-        print(request.POST)
-        pl=request.POST
-        form=Message(sender=request.user, reciever=User.objects.get(id=id),text=request.POST.get('messa'))
-        form.save()
-        print('sav')
-        messages.success(request,'message has been submitted!')
+
     chats=(Message.objects.filter(sender=request.user, reciever=User.objects.get(id=id))|Message.objects.filter(reciever=request.user, sender=User.objects.get(id=id))).order_by('timestamp')
     return render(request,'chat/chat.html',{'chats':chats,'id':id})
